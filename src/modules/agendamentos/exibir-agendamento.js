0eeb2d74feebbc15270c722547a1738b8b017aa6
Philipi Pastor <philiipastor123@gmail.com>
2026-04-23 17:38:49 -0300

    refactor: remove comentários desnecessários

--- src/modules/agendamentos/exibir-agendamento.js
+++ src/modules/agendamentos/exibir-agendamento.js
@@ -4,15 +4,12 @@ const periodNight = document.getElementById("period-night")
 
 export function periodo(agendamentoDia){
     
-    //Limpa o input a cada momento que é alterada a data
     periodMorning.innerHTML = ""
     periodAfternoon.innerHTML = ""
     periodNight.innerHTML = ""
-
-    //Percorre todos os agedamentos criados
+    
     agendamentoDia.forEach(agenda => {
 
-        //Cria os elementos para ser exebido na tela de agendamento
         const li = document.createElement("li")
         const spanHour = document.createElement("span")       
         const spanPet = document.createElement("span")
@@ -21,35 +18,31 @@ export function periodo(agendamentoDia){
         const spanRemove = document.createElement("span")
         
         li.setAttribute("data-id", agenda.id)
-
-        //Passa o conteúdo para os elementos 
+        
         spanHour.textContent = agenda.hora
         spanPet.textContent = agenda.nomePet
         smallNomeTutor.textContent = ` / ${agenda.nomeTutor}`
         spanService.textContent = agenda.servico
         spanRemove.textContent = "Remover agendamento"
 
-        //Adiciona classes aos elementos criados
         li.classList.add("item")
         spanHour.classList.add("time")
         spanPet.classList.add("pet")
         spanService.classList.add("service")
         spanRemove.classList.add("remove")
 
-        //Adiciona elementos filhos dentro de outros elementos pai
         spanPet.appendChild(smallNomeTutor)
         li.append(spanHour,spanPet,spanService,spanRemove)
 
-        //Foi criada uma condição para que os agendamentos fosse para seus determinados períodos
         if(agenda.hora >= "09:00"){
             periodMorning.append(li)
         }
 
-        else if(agenda.hora >= "13:00"){
+        if(agenda.hora >= "13:00"){
             periodAfternoon.append(li)
         }
 
-        else if(agenda.hora >= "19:00"){
+        if(agenda.hora >= "19:00"){
             periodNight.append(li)
         }
 
