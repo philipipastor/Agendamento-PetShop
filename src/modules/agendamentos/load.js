1e6a083bc62130dccb64cd71e73fc8a17c542df7
Philipi Pastor <philiipastor123@gmail.com>
2026-04-23 14:43:29 -0300

    chore: remove comentários desnecessários

--- src/modules/agendamentos/load.js
+++ src/modules/agendamentos/load.js
@@ -4,16 +4,13 @@ import { fetchDate } from "../../services/busca-data.js"
 const selectDate = document.querySelector("#date")
 
 export function eventoData() {
-    //Ao selecionar a data, os agendamentos são exibidos na tela
+    
     selectDate.addEventListener("change", async () => {
-        //Pega o valor do input da data selecionada
+       
         const date = selectDate.value
 
-        //Chama a nossa requisão com os dados dos agendamentos do dia selecionado
         const dadosAgendamento = await fetchDate({ date })
-        console.log(dadosAgendamento)
-
-        //Passa esses dados para a função criada para mostrar os dados na tela
+        
         periodo(dadosAgendamento)
     })
 }
