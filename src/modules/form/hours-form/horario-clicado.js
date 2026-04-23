f25ae86706c2a7ec788e142044b561e513d54b0d
Philipi Pastor <philiipastor123@gmail.com>
2026-04-23 14:39:56 -0300

    refactor: corrige caminho de import

---
+++ src/modules/form/hours-form/horario-clicado.js
@@ -0,0 +1,34 @@
+import { fetchDate } from "../../../services/busca-data.js";
+
+const dateForm = document.querySelector("#dateForm")
+
+//Seleciona a data no form
+dateForm.addEventListener("change", async () => {
+    const date = dateForm.value
+
+    const agendamentos = await fetchDate({date})
+
+    //Retorna apenas os horários do meu aquivo json
+    const horariosOcupados = agendamentos.map(ag => ag.hora)
+
+    const optionHora = document.querySelectorAll("#hora option")
+
+    optionHora.forEach(option => {
+        //se o horário selecionado estiver dentro de horários ocupados, a opção é desligada e é adicionado um ocupado
+        if(horariosOcupados.includes(option.value)){
+            option.disabled = true
+            option.textContent = `${option.value} - ocupado`
+        }
+
+        //se o horário selecionado estiver livre, vai continuar normal
+        else{
+            option.disabled = false
+            option.textContent = option.value
+        }
+    })
+})
+
+
+
+
+
